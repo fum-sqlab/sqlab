b54ef9fd0d3fc95b41eb56af1b36fb147bf8ca34
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-05-07 19:50:15 +0430

    Update Form Class

--- form_generator/models.py
+++ form_generator/models.py
@@ -3,7 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Form(models.Model):
-    pass
+    id = models.BigAutoField(unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200, blank=True, null=True)
+    fields = models.ManyToManyField('Field', through='Form_Field')
+    visible = models.BooleanField(default=True)
+    enable = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey('User', on_delete=models.SET_NULL,
+                                   null=True, blank=True)
+    updated_by = models.ForeignKey('User', on_delete=models.SET_NULL,
+                                   null=True, blank=True)
+
 
 class Field(models.Model):
     pass
