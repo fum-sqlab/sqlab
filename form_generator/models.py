d5c7fd1977868ac8f2c447e05578c41df80508f1
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-05-07 19:57:48 +0430

    Create Field_Form Class and add some comments to classes

--- form_generator/models.py
+++ form_generator/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Form(models.Model):
+    '''
+    Model for each Form
+    '''
     id = models.BigAutoField(unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     title = models.CharField(max_length=50)
@@ -19,6 +22,9 @@ class Form(models.Model):
 
 
 class Field(models.Model):
+    '''
+    Model For each data field
+    '''
     id = models.BigAutoField(unique=True)
     name = models.CharField(max_length=50)
     label = models.CharField(max_length=50)
@@ -31,3 +37,22 @@ class Field(models.Model):
     required = models.BooleanField(default=False)
     enable = models.BooleanField(default=True)
     visible = models.BooleanField(default=True)
+
+class Form_Field(models.Model):
+    '''
+    Model for connection between Form and Field table
+    '''
+    id = models.BigAutoField(primary_key = True)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    name = models.CharField(default=None, null=True, blank=True, max_length=50)
+    label = models.CharField(default=None, null=True, blank=True,max_length=50)
+    description = models.CharField(default=None, max_length=100, null=True, blank=True)
+    help_text = models.TextField(default=None, null=True, blank=True, max_length=200)
+    min_value = models.CharField(default=None, null=True, blank=True, max_length=50)
+    max_value = models.CharField(default=None, null=True, blank=True, max_length=50)
+    default_value = models.CharField(default=None, null=True, blank=True, max_length=50)
+    placeHolder = models.CharField(max_length=100)
+    required = models.BooleanField(default=None, null=True, blank=True)
+    enable = models.BooleanField(default=None, null=True, blank=True)
+    visible = models.BooleanField(default=None, null=True, blank=True)
