a40f4d0ff952c61ce07a109294b9dea1bf5b118b
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-25 22:25:05 +0430

    update views.py

--- form_generator/views.py
+++ form_generator/views.py
@@ -8,6 +8,15 @@ from rest_framework.response import Response
 from form_generator.models import Form
 from form_generator.serializers import FormSerializer
 
+def get_object(request, primary_key):
+    '''
+        Retrive a specific object
+    '''
+    try:
+        return Form.objects.get(pk=primary_key)
+    except Form.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
 @api_view(['GET', 'POST'])
 def form_list(request):
     '''
@@ -25,5 +34,35 @@ def form_list(request):
             return Response(form_serializer.data, status=status.HTTP_201_CREATED)
         return Response(form_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    else:
-        return Response()
+@api_view(['GET'])
+def get_specific_form(request, primary_key):
+    '''
+        Retrive a specific form
+    '''
+    form = get_object(request, primary_key)
+    if request.method == 'GET':
+        form_serializer = FormSerializer(form, many=True)
+        return Response(form_serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['PUT'])
+def update_specific_form(request, primary_key):
+    '''
+        Update a specific form
+    '''
+    form = get_object(request, primary_key)
+    if request.method == 'PUT':
+        updated_form = FormSerializer(form, data=request.data)
+        if updated_form.is_valid:
+            updated_form.save()
+            return Response(updated_form.data)
+        return Response(updated_form.errors, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['DELETE'])
+def delete_specific_form(request, primary_key):
+    '''
+        Delete a specific form
+    '''
+    form = get_object(request, primary_key)
+    if request.method == 'DELETE':
+        form.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
