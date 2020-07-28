42105a30540470cb67cf79e3e998e9f156178b08
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-28 18:26:43 +0430

    created API

--- form_generator/urls.py
+++ form_generator/urls.py
@@ -1,12 +1,11 @@
-'''
-    Created By Sara-Bolouri
-    Created At 07-26-2020
-'''
 from django.urls import path
-from .views import form_list
+from .views import get_forms, create_new_form, delete_form, update_form
 
 app_name = 'form_generator'
 
 urlpatterns = [
-    path('formlist/', form_list),
+    path('get/', get_forms, name='get_forms'),
+    path('create/', create_new_form, name='create_form'),
+    path('delete/<int:primary_key>/', delete_form, name='delete_form'),
+    path('update/<int:primary_key>/', update_form, name='update_form')
 ]
