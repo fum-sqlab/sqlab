0f711ce17412a7baf35ea2829a9eb874dd8d5933
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-28 18:26:30 +0430

    implement get(), delete(), update(), create()

--- form_generator/views.py
+++ form_generator/views.py
@@ -2,13 +2,14 @@
     Created By Sara-Bolouri
     Created At 07-25-2020
 '''
+from form_generator.models import Form
+from form_generator.serializers import FormSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from form_generator.models import Form
-from form_generator.serializers import FormSerializer
 
-def get_object(request, primary_key):
+
+def get_object(primary_key):
     '''
         Retrive a specific object
     '''
@@ -17,53 +18,36 @@ def get_object(request, primary_key):
     except Form.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET', 'POST'])
-def form_list(request):
-    '''
-        List all Forms, or Create a new Form
-    '''
-    if request.method == 'GET':
-        forms = Form.objects.all()
-        form_serializer = FormSerializer(forms, many=True)
-        return Response(form_serializer.data)
-
-    elif request.method == 'POST':
-        form_serializer = FormSerializer(data=request.data)
-        if form_serializer.is_valid:
-            form_serializer.save()
-            return Response(form_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(form_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(['GET'])
-def get_specific_form(request, primary_key):
-    '''
-        Retrive a specific form
-    '''
-    form = get_object(request, primary_key)
-    if request.method == 'GET':
-        form_serializer = FormSerializer(form, many=True)
-        return Response(form_serializer.data, status=status.HTTP_200_OK)
+def get_forms(requesrt):
+    if requesrt.method == 'GET':
+        forms = Form.objects.all()
+        forms_serializer = FormSerializer(forms, many=True)
+        return Response(forms_serializer.data, status=status.HTTP_200_OK)   
 
-@api_view(['PUT'])
-def update_specific_form(request, primary_key):
-    '''
-        Update a specific form
-    '''
-    form = get_object(request, primary_key)
-    if request.method == 'PUT':
-        updated_form = FormSerializer(form, data=request.data)
-        if updated_form.is_valid:
-            updated_form.save()
-            return Response(updated_form.data)
-        return Response(updated_form.errors, status=status.HTTP_404_NOT_FOUND)
+@api_view(['POST'])
+def create_new_form(requesrt):
+    form_serializer = FormSerializer(data=request.data)
+    if form_serializer.is_valid:
+        form_serializer.save()
+        return Response(form_serializer.data, status=status.HTTP_201_CREATED)
+    return Response(form_serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
 
 @api_view(['DELETE'])
-def delete_specific_form(request, primary_key):
-    '''
-        Delete a specific form
-    '''
-    form = get_object(request, primary_key)
-    if request.method == 'DELETE':
-        form.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+def delete_form(requesrt, primary_key):
+    form = get_object(primary_key=primary_key)
+    form.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)  
+                
+@api_view(['PUT'])
+def update_form(requesrt, primary_key):
+    form = get_object(primary_key=primary_key)
+    updated_form = FormSerializer(form, data=requesrt.data)
+    if updated_form.is_valid():
+        updated_form.save()
+        return Response(updated_form.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
         
+    
+    
+    
