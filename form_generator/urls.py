d1af740437cfdeb98635c5242ba48981e2fdbac0
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-28 18:30:33 +0430

    add comments

--- form_generator/urls.py
+++ form_generator/urls.py
@@ -4,8 +4,8 @@ from .views import get_forms, create_new_form, delete_form, update_form
 app_name = 'form_generator'
 
 urlpatterns = [
-    path('get/', get_forms, name='get_forms'),
-    path('create/', create_new_form, name='create_form'),
-    path('delete/<int:primary_key>/', delete_form, name='delete_form'),
-    path('update/<int:primary_key>/', update_form, name='update_form')
+    path('get/', get_forms, name='get_forms'),                          #http://127.0.0.1:8000/form/get/
+    path('create/', create_new_form, name='create_form'),               #http://127.0.0.1:8000/form/create/
+    path('delete/<int:primary_key>/', delete_form, name='delete_form'), #http://127.0.0.1:8000/form/delete/PK/
+    path('update/<int:primary_key>/', update_form, name='update_form'), #http://127.0.0.1:8000/form/update/PK/
 ]
