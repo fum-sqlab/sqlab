a967019bdd33cc86402f22055a74f3d56f219239
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-28 18:44:37 +0430

    update views.py

--- form_generator/views.py
+++ form_generator/views.py
@@ -19,30 +19,30 @@ def get_object(primary_key):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET'])
-def get_forms(requesrt):
+def get_forms(request):
     if requesrt.method == 'GET':
         forms = Form.objects.all()
         forms_serializer = FormSerializer(forms, many=True)
         return Response(forms_serializer.data, status=status.HTTP_200_OK)   
 
 @api_view(['POST'])
-def create_new_form(requesrt):
+def create_new_form(request):
     form_serializer = FormSerializer(data=request.data)
-    if form_serializer.is_valid:
+    if form_serializer.is_valid():
         form_serializer.save()
         return Response(form_serializer.data, status=status.HTTP_201_CREATED)
-    return Response(form_serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
+    return Response(form_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
-def delete_form(requesrt, primary_key):
+def delete_form(request, primary_key):
     form = get_object(primary_key=primary_key)
     form.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)  
                 
 @api_view(['PUT'])
-def update_form(requesrt, primary_key):
+def update_form(request, primary_key):
     form = get_object(primary_key=primary_key)
-    updated_form = FormSerializer(form, data=requesrt.data)
+    updated_form = FormSerializer(form, data=request.data)
     if updated_form.is_valid():
         updated_form.save()
         return Response(updated_form.data)
