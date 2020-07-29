5ed952bd5c3594f919073f1b87c29c5956fc33e9
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-29 18:49:20 +0430

    update urls.py

--- form_generator/urls.py
+++ form_generator/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import get_forms, create_new_form, delete_form, update_form, create_page
+from .views import *
+                   
 
 app_name = 'form_generator'
 
 urlpatterns = [
-    path('get/', get_forms, name='get_forms'),                          #http://127.0.0.1:8000/form/get/
-    path('create/', create_new_form, name='create_form'),               #http://127.0.0.1:8000/form/create/
-    path('delete/<int:primary_key>/', delete_form, name='delete_form'), #http://127.0.0.1:8000/form/delete/PK/
-    path('update/<int:primary_key>/', update_form, name='update_form'), #http://127.0.0.1:8000/form/update/PK/
-    path('create_page/', create_page, name='create_page')                #http://127.0.0.1:8000/page/create_page/
+    path('form/', FormView.as_view({'get':'list', 'post':'create'}) , name='get_create_forms'),
+    path('form/<int:pk>/', FormView.as_view({'delete':'destroy', 'put':'update'}), name='delete_update_forms'),
+    path('group/', GroupView.as_view({'get':'list', 'post':'create'}) , name='get_create_forms'),
+
+
+    # path('create_page/', create_page, name='create_page'),
+    # path('add_form/<int:page_pk>/<int:section_pk>/<int:form_pk>/', add_form, name='add_form'),
+    # path('add_form_to_gp/<int:gp_pk>/<int:form_pk>/', add_form_to_group, name='add_form_to_gp')
 ]
