5a9f93035d3242d56ea9a7e19381ca2c8c91e980
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-29 23:02:52 +0430

    add url for remove a form from a group

--- form_generator/urls.py
+++ form_generator/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('form/', FormView.as_view({'get':'list', 'post':'create'}) , name='get_create_forms'),
     path('form/<int:pk>/', FormView.as_view({'delete':'destroy', 'put':'update'}), name='delete_update_forms'),
     path('group/', GroupView.as_view({'get':'list', 'post':'create'}) , name='get_create_forms'),
-    path('set_group/<int:gp_pk>/<int:form_pk>/', GroupView.as_view({'post':'add_form_to_group'}), name='set_form_to_gp')
+    path('set_to_group/gp<int:gp_pk>/form<int:form_pk>/', GroupView.as_view({'put':'add_form_to_group'}), name='set_form_to_gp'),
+    path('remove_from_group/<int:gp_pk>/<int:form_pk>/', GroupView.as_view({'put':'remove_form_from_grou'}, name='remove_form_from_gp'))
 ]
