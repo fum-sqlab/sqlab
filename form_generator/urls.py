780ffbfa6c8e5128f802deed668ac77c1c2997d8
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-29 23:22:30 +0430

    add new path() for destroy group

--- form_generator/urls.py
+++ form_generator/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('form/', FormView.as_view({'get':'list', 'post':'create'}) , name='get_create_forms'),
     path('form/<int:pk>/', FormView.as_view({'delete':'destroy', 'put':'update'}), name='delete_update_forms'),
     path('group/', GroupView.as_view({'get':'list', 'post':'create'}) , name='get_create_forms'),
+    path('group/<int:pk>/', GroupView.as_view({'delete':'destroy'}), name='delete_group'),
     path('set_to_group/gp<int:gp_pk>/form<int:form_pk>/', GroupView.as_view({'put':'add_form_to_group'}), name='set_form_to_gp'),
     path('remove_from_group/<int:gp_pk>/<int:form_pk>/', GroupView.as_view({'put':'remove_form_from_grou'}, name='remove_form_from_gp'))
 ]
