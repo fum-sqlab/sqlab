62a5886ef182b33ff8d2d63fb02efd390676f32f
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-29 23:03:12 +0430

    add remove_form_from_group() mrthod

--- form_generator/views.py
+++ form_generator/views.py
@@ -2,20 +2,13 @@
     Created By Sara-Bolouri
     Created At 07-25-2020
 '''
-from form_generator.models import Form, Page, Section, Group
+from form_generator.models import Form, Page, Section, Group, GroupForm
 from form_generator.serializers import FormSerializer, PageSerializer, SectionSerializer, GroupSerializer
 from rest_framework import status, viewsets
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
+from .helper import get_group_object, get_form_object
 
-def get_object(primary_key):
-    '''
-        Retrive a specific object
-    '''
-    try:
-        return Form.objects.get(pk=primary_key)
-    except Form.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
 class FormView(viewsets.ViewSet):
 
@@ -41,7 +34,7 @@ class FormView(viewsets.ViewSet):
         '''
             Delete form object that its 'id' is 'pk'
         '''
-        form = get_object(primary_key=pk)
+        form = get_form_object(primary_key=pk)
         form.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -49,7 +42,7 @@ class FormView(viewsets.ViewSet):
         '''
             Update form object that its 'id' is 'pk'
         '''
-        form = get_object(primary_key=pk)
+        form = get_form_object(primary_key=pk)
         updated_form = FormSerializer(form, data=request.data)
         if updated_form.is_valid():
             updated_form.save()
@@ -70,17 +63,25 @@ class GroupView(viewsets.ViewSet):
             return Response(group_serializer.data, status=status.HTTP_201_CREATED)
         return Response(group_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=True, methods=['POST'])
+    @action(detail=True, methods=['PUT'])
     def add_form_to_group(self, request, gp_pk, form_pk):
-        try:
-            group = Group.objects.get(pk=gp_pk)
-            form = Form.objects.get(pk=form_pk)
-        except Group.DoesNotExist or Form.DoesNotExist:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        '''
+            Set a form to a specific group
+        '''
+        form = get_form_object(primary_key=form_pk)
+        group = get_group_object(primary_key=gp_pk)
         lst = [form]
         group.form.add(*lst)
         group.save()
-        return Response(status=status.HTTP_200_OK) 
+        return Response(status=status.HTTP_200_OK)
+    
+    @action(detail=True, methods=['PUT'])
+    def remove_form_from_grou(self, request, gp_pk, form_pk):
+        try:
+            GroupForm.objects.filter(form=form_pk, group=gp_pk).delete()
+            return Response({"message":"Done"}, status=status.HTTP_200_OK)
+        except GroupForm.DoesNotExist:
+            return Response({"message":"No data"}, status=status.HTTP_404_NOT_FOUND)
 
 
 
@@ -101,7 +102,7 @@ def create_section(request):
     return Response(section_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT'])
-def add_form(requesrt, page_pk, section_pk, form_pk):
+def add_form(request, page_pk, section_pk, form_pk):
     try:
         page = Page.objects.get(pk=page_pk)
         form = Form.objects.get(pk=form_pk)
@@ -116,7 +117,3 @@ def add_form(requesrt, page_pk, section_pk, form_pk):
     page.sections.add(*lst)
     page.save()
     return Response(page, status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-def testing(request):
-    pass
