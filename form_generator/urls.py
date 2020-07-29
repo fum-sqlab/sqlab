ea173890ec2b0418985033d3256797434795ef44
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-07-29 20:04:41 +0430

    optimised urls.py

--- form_generator/urls.py
+++ form_generator/urls.py
@@ -8,9 +8,5 @@ urlpatterns = [
     path('form/', FormView.as_view({'get':'list', 'post':'create'}) , name='get_create_forms'),
     path('form/<int:pk>/', FormView.as_view({'delete':'destroy', 'put':'update'}), name='delete_update_forms'),
     path('group/', GroupView.as_view({'get':'list', 'post':'create'}) , name='get_create_forms'),
-
-
-    # path('create_page/', create_page, name='create_page'),
-    # path('add_form/<int:page_pk>/<int:section_pk>/<int:form_pk>/', add_form, name='add_form'),
-    # path('add_form_to_gp/<int:gp_pk>/<int:form_pk>/', add_form_to_group, name='add_form_to_gp')
+    path('set_group/<int:gp_pk>/<int:form_pk>/', GroupView.as_view({'post':'add_form_to_group'}), name='set_form_to_gp')
 ]
