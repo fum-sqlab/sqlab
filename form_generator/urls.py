b63a96782de0d8dc66e61d0ff1a2d1cbec26ecf7
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-08-03 19:11:58 +0430

    update urls.py

--- form_generator/urls.py
+++ form_generator/urls.py
@@ -6,12 +6,13 @@ app_name = 'form_generator'
 
 urlpatterns = [
     path('form/', FormView.as_view({'get':'list', 'post':'create'}) , name='get_create_form'),
-    path('form/<int:pk>/', FormView.as_view({'delete':'destroy', 'put':'update'}), name='delete_update_forms'),
-    path('form/<int:field_pk>/<int:form_pk>/', FormView.as_view({'put':'add_field_to_form'}), name='set_field'),
+    path('form/<int:pk>/', FormView.as_view({'delete':'destroy', 'put':'update', 'get':'show_form_details'}), name='delete_update_forms'),
+    path('form/field_delete/<int:ff_id>/', FormView.as_view({'put':'add_field_to_form'}), name='set_field'),
+
 
     path('group/', GroupView.as_view({'get':'list', 'post':'create'}) , name='get_create_group'),
     path('group/<int:pk>/', GroupView.as_view({'delete':'destroy'}), name='delete_group'),
-    path('group/<int:gp_pk>/<int:form_pk>/', GroupView.as_view({'put':'add_form_to_group', 'delete':'remove_form_from_grou'}),
+    path('group/<int:gp_pk>/<int:form_pk>/', GroupView.as_view({'put':'add_form_to_group', 'delete':'remove_form_from_group'}),
          name='set_remove_form'),
    
     path('page/', PageView.as_view({'get':'list', 'post':'create'}), name='get_create_page'),
