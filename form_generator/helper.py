775de7b471125587622ce54e83bae6de6fcccf1f
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-08-10 20:42:15 +0430

    add new methods

--- form_generator/helper.py
+++ form_generator/helper.py
@@ -1,4 +1,5 @@
 from .models import *
+from .serializers import FormFieldSerializer
 from .exception import exceptions
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
@@ -12,7 +13,8 @@ TYPES = {
     "group": Group,
     "formfield" : FormField,
     "pageform" : PageForm,
-    "groupform" : GroupForm
+    "groupform" : GroupForm,
+    "submission" : Submission
 }
 
 def get_object(type_object="", primary_key=None):
@@ -21,6 +23,13 @@ def get_object(type_object="", primary_key=None):
         return obj.objects.get(pk=primary_key)
     except ObjectDoesNotExist:
         raise exceptions(type_object)
+
+def get_object_or_None(type_object="", primary_key=None):
+    obj = TYPES[type_object]
+    try:
+        return obj.objects.get(pk=primary_key)
+    except ObjectDoesNotExist:
+        return None
 
 def filter_object(type_object="", **kwargs):
     obj = TYPES[type_object]
@@ -35,3 +44,21 @@ def filter_for_deleting(type_object="", **kwargs):
     if not result.exists():
         return None
     return result
+
+def list_required_field(form_id):
+    _fields = FormFieldSerializer(filter_object(type_object="formfield", form=form_id), many=True)
+    required = []
+    for item in _fields.data:
+        if item["required"] == True:
+            required.append(item["id"])   
+    return required
+
+def checking_requirement(requied_field, answers):
+    '''
+        If all required fields were answered by user, return TRUE else return FALSE
+    '''
+    for answer in answers:
+        for rf in requied_field:
+            if answer["field"] == rf:
+                requied_field.remove(rf)
+    return requied_field
