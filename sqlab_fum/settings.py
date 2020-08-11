54b295cf93580f5deb279ec728b3e17bcbb94c83
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-08-11 19:30:18 +0430

    add "corsheaders" app

--- sqlab_fum/settings.py
+++ sqlab_fum/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 
     'base.apps.BaseConfig',
     'form_generator.apps.FormGeneratorConfig',
@@ -55,6 +56,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'sqlab_fum.urls'
@@ -134,3 +138,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True
