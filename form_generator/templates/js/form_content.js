017fc42e7cb4a1544b70165f4b7c4c000e449a12
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-08-12 13:39:30 +0430

    Updated

--- form_generator/templates/js/form_content.js
+++ form_generator/templates/js/form_content.js
@@ -1,27 +1,57 @@
 function localFormContent(){
-    var form_id_val =  document.getElementById("form_id").value;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        form = createForm(myObj);
-        document.getElementById("form_demo").innerHTML = form;
-      }
-    };
-    var url = "http://127.0.0.1:8000/form/" + form_id_val + "/";
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+  var form_id_val =  document.getElementById("form_id").value;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+      form = createForm(myObj);
+      document.getElementById("form_demo").innerHTML = form;
+    }
+  };
+  var url = "http://127.0.0.1:8000/form/" + form_id_val + "/";
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
 }
 
-function createForm(myObj) {
-    text = "<form>"
-      for(i = 0; i< myObj.fields.length; i++){
-        text += "<label for=\"" + myObj.fields[i].id + "\">" + myObj.fields[i].label + "</label><br>";
-        text += "<input type=\"" + myObj.fields[i].field_type + "\"" +
-                "id=\"" + myObj.fields[i].id + "\"" +
-                "name=\"" + myObj.fields[i].name + "\"" +
-                "value=\""+ myObj.fields[i].default_value + "\">" + "<br>";
-      } 
-      text += "</form>";
-      return text;
+function createForm(_myObj) {
+  text = "<form>"
+    myObj = sort(_myObj.fields)
+    for(i = 0; i< myObj.length; i++){
+      if(myObj[i].field_type == "checkbox"){
+        text += checkbox_field(myObj[i])
+      }
+      else{
+        text += "<label for=\"" + myObj[i].id + "\">" + myObj[i].label + "</label><br>";
+        text += "<input type=\"" + myObj[i].field_type + "\"" +
+                "id=\"" + myObj[i].id + "\"" +
+                "name=\"" + myObj[i].name + "\"" +
+                "value=\""+ myObj[i].default_value + "\">" + "<br>";
+      }
+    } 
+  text += "</form>";
+  return text;
+}
+
+function checkbox_field(data){
+  check = '<input type="checkbox" id="' + data.id + 
+          '" name="' + data.name + 
+          '" value="' + data.default_value + '">';
+  check += '<label for="' + data.id + '">' +
+            data.label +
+            '</label><br>';
+  
+  return check;
+}
+
+function sort(data_list){
+  for(i=0; i<data_list.length; i++){
+    for(j=i+1; j<data_list.length; j++){
+      if(data_list[i].placeHolder > data_list[j].placeHolder){
+        k = data_list[i];
+        data_list[i] = data_list[j];
+        data_list[j] = k;
+      }
+    }
+  }
+  return data_list;
 }
