bccd32fca5e6a6cb368db2eb66a68b6c9fc1d4e1
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-08-12 17:06:33 +0430

    add delete template

---
+++ form_generator/templates/js/form_delete.js
@@ -0,0 +1,14 @@
+function localDeleteForm(){
+    var form_id_val =  document.getElementById("form_id").value;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        // form = createForm(myObj);
+        document.getElementById("form_demo").innerHTML = JSON.stringify(myObj);
+      }
+    };
+    var url = "http://127.0.0.1:8000/form/" + form_id_val + "/";
+    xmlhttp.open("DELETE", url, true);
+    xmlhttp.send(null);
+}
