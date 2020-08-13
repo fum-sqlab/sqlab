7998d1b17278e3447b882479b1fdf15cec37c752
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-08-13 20:31:50 +0430

    add field api

--- form_generator/urls.py
+++ form_generator/urls.py
@@ -5,22 +5,24 @@ from .views import *
 app_name = 'form_generator'
 
 urlpatterns = [
-    path('form/', FormView.as_view({'get':'list', 'post':'create'}) , name='get_create_form'),
-    path('form/<int:pk>/', FormView.as_view({'delete':'destroy', 'put':'update', 'get':'show_form_details'}),
+     path('field/', FieldView.as_view({'get':'list'}), name='get_fields'),
+
+     path('form/', FormView.as_view({'get':'list', 'post':'create'}) , name='get_create_form'),
+     path('form/<int:pk>/', FormView.as_view({'delete':'destroy', 'put':'update', 'get':'show_form_details'}),
          name='delete_update_forms'),
-    path('form/remove_field/<int:ff_id>/', FormView.as_view({'delete':'remove_field_from_form'}), name='remove_field'),
-    path('form/set_field/<int:field_pk>/<int:form_pk>/', FormView.as_view({'put':'add_field_to_form'}), name='set_field'),
+     path('form/remove_field/<int:ff_id>/', FormView.as_view({'delete':'remove_field_from_form'}), name='remove_field'),
+     path('form/set_field/<int:field_pk>/<int:form_pk>/', FormView.as_view({'put':'add_field_to_form'}), name='set_field'),
 
 
-    path('group/', GroupView.as_view({'get':'list', 'post':'create'}) , name='get_create_group'),
-    path('group/<int:pk>/', GroupView.as_view({'delete':'destroy', 'get':'retrive'}), name='delete_group'),
-    path('group/<int:gp_pk>/<int:form_pk>/', GroupView.as_view({'put':'add_form_to_group', 'delete':'remove_form_from_group'}),
+     path('group/', GroupView.as_view({'get':'list', 'post':'create'}) , name='get_create_group'),
+     path('group/<int:pk>/', GroupView.as_view({'delete':'destroy', 'get':'retrive'}), name='delete_group'),
+     path('group/<int:gp_pk>/<int:form_pk>/', GroupView.as_view({'put':'add_form_to_group', 'delete':'remove_form_from_group'}),
          name='set_remove_form'),
    
-    path('page/', PageView.as_view({'get':'list', 'post':'create'}), name='get_create_page'),
-    path('page/<int:pk>/', PageView.as_view({'delete':'destroy', 'get':'show_page_details'}), name='delete_page'),
-    path('page/<int:page_pk>/<int:form_pk>/', PageView.as_view({'put':'add_form_to_page'}), name='set_form_to_page'),
-    path('page/<int:page_pk>/<int:form_pk>/<int:section_pk>/', PageView.as_view({'delete':'remove_form_from_page'}),
+     path('page/', PageView.as_view({'get':'list', 'post':'create'}), name='get_create_page'),
+     path('page/<int:pk>/', PageView.as_view({'delete':'destroy', 'get':'show_page_details'}), name='delete_page'),
+     path('page/<int:page_pk>/<int:form_pk>/', PageView.as_view({'put':'add_form_to_page'}), name='set_form_to_page'),
+     path('page/<int:page_pk>/<int:form_pk>/<int:section_pk>/', PageView.as_view({'delete':'remove_form_from_page'}),
          name='remove_form'),
 
      path('submit/', AnswerView.as_view({'post':'set_answer'}), name='set_answer')
