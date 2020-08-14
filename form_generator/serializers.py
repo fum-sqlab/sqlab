1959ea876a9e9f84b973605aa26142d6e47dacb0
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-08-14 19:29:33 +0430

    deleted updated method in FormSerializer class

--- form_generator/serializers.py
+++ form_generator/serializers.py
@@ -37,39 +37,6 @@ class FormSerializer(serializers.ModelSerializer):
     def create(self, valide_data):
         return Form.objects.create(**valide_data)
 
-    def update(self, instance, new_valid_data):
-
-        all_fields = new_valid_data.get('fields')
-        instance.title = new_valid_data.get('title')
-        instance.description = new_valid_data.get('description')
-        instance.visible = new_valid_data.get('visible')
-        instance.enable = new_valid_data.get('enable')
-        instance.created_by = new_valid_data.get('created_by')
-        instance.updated_by = new_valid_data.get('updated_by')
-        instance.save()
-
-        for field in all_fields:
-            field_id = field.get('id')
-            try:
-                field_obj = Field.objects.get(pk=field_id)
-            except Field.DoesNotExist:
-                pass
-
-            field_obj.name = field.get('name')
-            field_obj.label = field.get('label')
-            field_obj.description = field.get('description')
-            field_obj.help_text = field.get('help_text')
-            field_obj.field_type = field.get('field_type')
-            field_obj.min_value = field.get('min_value')
-            field_obj.max_value = field.get('max_value')
-            field_obj.default_value = field.get('default_value')
-            field_obj.required = field.get('required')
-            field_obj.enable = field.get('enable')
-            field_obj.visible = field.get('visible')
-            field_obj.save()
-        
-        return instance
-
 class GroupSerializer(serializers.ModelSerializer):
     form = FormSerializer(many=True, read_only=True)
     class Meta:
