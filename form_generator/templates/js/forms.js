6edd486b05e0ffe70903ff23e40f17ce36743f33
Sara Bolouri <sara.bolouri@mail.um.ac.ir>
2020-08-14 11:19:53 +0430

    updated

--- form_generator/templates/js/forms.js
+++ form_generator/templates/js/forms.js
@@ -3,8 +3,7 @@ function loadForms() {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           var all_forms = JSON.parse(this.responseText);
-          return create_list(all_forms);
-          
+          create_list(all_forms);        
       }
     };
     xhttp.open("GET", "http://127.0.0.1:8000/form/", true);
@@ -12,14 +11,15 @@ function loadForms() {
 }
 
 function create_list(data) {
-    var table="<tr><th>ID</th><th>Title</th></tr>";
+    var table= "<table id=\"myTable\">";
+    table += "<tr><th>ID</th><th>Title</th></tr>";
     for (i = 0; i <data.length; i++) { 
-      table +=    "<tr><td>" +
-                      data[i].id +
-                  "</td><td>" +
-                      data[i].title +
-                  "</td></tr>";
+        table +=    "<tr><td>" +
+                        data[i].id +
+                    "</td><td>" +
+                        data[i].title +
+                    "</td></tr>";
     }
-    document.getElementById("form_table").innerHTML = table;
-    return table;
+    table += "</table>";
+    document.getElementById("here").innerHTML = table;
 }
